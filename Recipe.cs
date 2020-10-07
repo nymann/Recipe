1068cd3dbfd1e18ad5b4288bf2c392ce998c87a8
Kristian Nymann <kristian@nymann.dev>
2020-10-08 01:43:45 +0200

    Add base project structure

---
+++ Recipe.cs
@@ -0,0 +1,31 @@
+﻿using ExileCore;
+
+namespace Recipe
+{
+    public class Recipe : BaseSettingsPlugin<Core.Config>
+    {
+        public Recipe()
+        {
+            Name = "Recipe";
+        }
+
+        public override bool Initialise()
+        {
+            Settings.Enable.OnValueChanged += (sender, enabled) =>
+            {
+                if (enabled)
+                {
+                    // Setup
+                    DebugWindow.LogMsg($"{Name}: enabled", 5);
+                }
+                else
+                {
+                    // Teardown
+                    DebugWindow.LogMsg($"{Name}: disabled", 5);
+                }
+            };
+
+            return true;
+        }
+    }
+}
